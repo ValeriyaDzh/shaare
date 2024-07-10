418113cf4e258efe2ffed9878d4b09624c18862d
Valeriya <Dzhavadova.v@yandex.ru>
2024-07-10 18:45:48 +0300

    add InvalidCredentials Exception

--- backend/src/exeptions.py
+++ backend/src/exeptions.py
@@ -17,3 +17,9 @@ class AlreadyExists(HTTPException):
 
     def __init__(self, detail: str = "Already exists"):
         super().__init__(status_code=status.HTTP_409_CONFLICT, detail=detail)
+
+
+class InvalidCredentialsException(HTTPException):
+
+    def __init__(self, detail: str = "Could not validate credentials") -> None:
+        super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, detail=detail)
