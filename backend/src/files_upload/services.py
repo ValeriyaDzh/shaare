fe603bf4f009b44fdeeb701acc048893ece74ec7
Valeriya <Dzhavadova.v@yandex.ru>
2024-07-17 15:20:36 +0300

    add get_file_by_name

--- backend/src/files_upload/services.py
+++ backend/src/files_upload/services.py
@@ -16,33 +16,43 @@ class FileUploadService(BaseRepository):
     def __init__(self, session, model=FileUpload):
         super().__init__(model=model, session=session)
 
+    async def get_file_by_name(self, filename: str) -> FileUpload:
+        file = await self.get("filename", filename)
+        return file
+
     async def upload(self, data: UploadFile, user_id: UUID) -> FileUpload:
+        path = await self._save_to_server(data.filename, data.file)
 
-        if await self._save_to_server(data.filename, data.file):
+        if path:
             uploaded_file = await self.add_to_database(
-                user_id, data.filename, data.size
+                user_id, data.filename, path, data.size
             )
-
             return uploaded_file
 
     @staticmethod
-    async def _save_to_server(filename: str, data: BinaryIO) -> bool:
+    async def _save_to_server(filename: str, data: BinaryIO) -> str:
         try:
-            path = f"{Path(os.getcwd())}/src//uploaded_files/{filename}"
+            path = f"{Path(os.getcwd())}/src/uploaded_files/{filename}"
             with open(path, "+wb") as p:
                 shutil.copyfileobj(data, p)
 
-            return os.path.exists(path)
+            if os.path.exists(path):
+                return path
 
         except Exception as e:
             logger.error(f"Error saving the file on the server: {e}")
 
     async def add_to_database(
-        self, user_id: UUID, filename: str, size_bytes: int
+        self, user_id: UUID, filename: str, file_path: str, size_bytes: int
     ) -> FileUpload:
 
         size_mb = self._bytes_to_megabytes(size_bytes)
-        file_dict = {"filename": filename, "user_id": user_id, "size_mb": size_mb}
+        file_dict = {
+            "filename": filename,
+            "user_id": user_id,
+            "file_path": file_path,
+            "size_mb": size_mb,
+        }
         uploaded_file = await self.save(file_dict)
 
         return uploaded_file
