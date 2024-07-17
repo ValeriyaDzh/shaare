eb0c19cdb5a4d47bb12ba3c924da853f33c21222
Valeriya <Dzhavadova.v@yandex.ru>
2024-07-17 15:19:22 +0300

    valid_filename dependency

--- backend/src/files_upload/dependencies.py
+++ backend/src/files_upload/dependencies.py
@@ -3,6 +3,8 @@ from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.database import get_session
+from src.exeptions import NotFoundException
+from src.files_upload.models import FileUpload
 from src.files_upload.services import FileUploadService
 
 logger = logging.getLogger(__name__)
@@ -10,3 +12,13 @@ logger = logging.getLogger(__name__)
 
 async def get_uploadfile_service(session: AsyncSession = Depends(get_session)):
     return FileUploadService(session=session)
+
+
+async def valid_filename(
+    file_name: str, file_service: FileUploadService = Depends(get_uploadfile_service)
+) -> FileUpload:
+
+    file = await file_service.get_file_by_name(file_name)
+    if file is None:
+        raise NotFoundException(f"Not found file: {file_name}")
+    return file
